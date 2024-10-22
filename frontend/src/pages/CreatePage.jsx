f67e9e9957a6dea33c4f8a988ed2efc5b203304b
tinxte <150742495+Tinxte@users.noreply.github.com>
2024-10-21 22:28:37 -0500

    added newJob state, started form ui

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { Container, useColorModeValue, Box, Heading, VStack } from '@chakra-ui/react'
+
 
 const CreatePage = () => {
+
+  const [newJob, setNewJob] = useState( {
+    jobName:"",
+    companyName:"",
+    website:"",
+});
+
   return (
-    <div>CreatePage</div>
+    <Container maxW={"container.sm"}>CreatePage
+    <VStack spacing={8}>
+      <Heading as={"h1"} size={"2x1"} textAlign={"center"} mb={8}>
+        Suggest New Job
+      </Heading>
+
+      <Box
+      w={"full"} bg={useColorModeValue("white", "gray.800")} 
+      p={6} 
+      rounded={"lg"}
+      shadow={"md"}
+      >
+
+        <VStack spacing={4}>
+
+          <Input placeholder="Job title"
+          name= "name"
+          value={newJob.jobName}/>
+
+        </VStack>
+
+      </Box>
+
+
+    </VStack>
+    
+    </Container>
   )
 }
 
